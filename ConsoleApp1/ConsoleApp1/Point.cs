1cf75e284950c09f44bab0c4dc5e6f48d8d4581b
ivlieva-anya <ivlieva.anya@mail.ru>
2019-01-07 13:30:45 +0300

    constructor

--- ConsoleApp1/ConsoleApp1/Point.cs
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -11,6 +11,12 @@ namespace ConsoleApp1
         public int x;
         public int y;
         public char sym;
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
